afa1894b352043d231b2fa3e796c13593f187683
guneyselAli <aliguneysel@gmail.com>
2023-01-19 19:52:41 +0300

    19.01.2023 commit

--- get_next_line.c
+++ get_next_line.c
@@ -22,7 +22,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	if (!s)
 		return (0);
-	sub = malloc(sizeof(char) * (len + 1));
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len <= ft_strlen(s) - start)
+		sub = malloc(sizeof(char) * (len + 1));
+	else
+		sub = malloc(sizeof(char) * (ft_strlen(s) - start + 1));
 	if (!sub)
 		return (0);
 	while (*(s + start) && (i < len))
@@ -36,29 +41,51 @@ int get_next_buffer(int fd, char *str)
 	int a;
 
 	a = read(fd, str, BUFFER_SIZE);
-	if (a == 0)
+	if (a <= 0)
 	{
-		if (*str != 0)
-			return (2);
-		free(str);
-		return(0);
-	}
-	if (a < 0)
-	{
-		free (str);
-		return (0);
+		*str = 0;
+		return(a);
 	}
 	*(str + a) = 0;
-	return (3);
+	return (a);
+}
+
+char	*new_str(int fd, char *str)
+{
+	char	*buffer;
+	char	*tmp;
+	int		checker;
+	
+	checker = 1;
+
+	while (ft_strchr(str, '\n') == 0 && checker > 0)
+	{
+		tmp = str;
+		buffer = malloc(BUFFER_SIZE + 1);
+		*buffer = 0;
+		checker = get_next_buffer(fd, buffer);
+		str = ft_strjoin(str, buffer);
+		free(buffer);
+		free(tmp);
+	}
+	if (checker < 0)
+	{
+		free(str);
+		return (0);
+	}		
+	if (checker == 0 && ft_strlen(str) == 0)
+	{
+		free(str);
+		return (0);
+	}
+	return (str);
 }
 
 char *get_next_line(int fd)
 {
 	static char	*str;
-	char 		*tmp;
+	char		*tmp;
 	char		*line;
-	int			checker;
-
 
 	if (!str)
 	{
@@ -67,27 +94,27 @@ char *get_next_line(int fd)
 	}
 	if (!str)
 		return (0);
-	if (!ft_strchr(str,'\n'))
-		checker = get_next_buffer(fd,str);
-	if (checker == 0)
+	str = new_str(fd,str);
+	if (!str)
 		return (0);
-	while (1)
+	if (ft_strchr(str, '\n'))
 	{
-		if (ft_strchr(str, '\n'))
-		{	
-			tmp = ft_strchr(str, '\n');
-			line = ft_substr(str, 0, tmp - str + 1);
-			str = ft_strdup(tmp + 1);
-			return (line);
-		}
-		tmp = malloc(BUFFER_SIZE + 1);
-		checker = get_next_buffer(fd,tmp);
-		str = ft_strjoin(str,tmp);
-		if (checker == 2)
-			return (str);
+		line = ft_substr(str, 0, ft_strchr(str, '\n') - str + 1);
+		tmp = str;
+		str = ft_strdup(ft_strchr(str, '\n') + 1);
+		free(tmp);
+		return (line);
 	}
+	if (ft_strlen(str))
+	{
+		tmp = str;
+		str = 0;
+		return (tmp);
+	}
+	return (0);
 }
 
+/*
 int main(void)
 {
 	int fd = open("alternate_line_nl_with_nl", O_RDWR);
@@ -97,6 +124,5 @@ int main(void)
 		printf("%s",a);
 		a = get_next_line(fd);
 	}
-	printf("%s", a);
-}
-
+	printf("%s",a);
+}*/
